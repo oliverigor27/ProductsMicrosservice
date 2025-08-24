67edaa2ec3886d8faa0f7f28c63d955716896139
Igor Oliveira <igoroliveira9827@gmail.com>
2025-08-24 14:33:05 -0300

    feat: add serilog and MiniProfiler to observallity

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,10 +1,21 @@
 using Ocelot.Middleware;
 using ProductsMicrosservice.Gateway.Extensions;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .CreateLogger();
 
 WebApplication BuildApp(string[] args)
 {
+    
     var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddOpenApi();
+    builder.Host.UseSerilog();
+    builder.Services.AddMiniProfiler(options => {
+        options.RouteBasePath = "/profiler";
+    });
     builder.OcelotServiceBuilder();
     return builder.Build();
 }
@@ -14,6 +25,7 @@ async Task RunApp(WebApplication app)
     if (app.Environment.IsDevelopment())
         app.MapOpenApi();
     app.UseHttpsRedirection();
+    app.UseMiniProfiler();
     await app.UseOcelot();
     app.Run();
 }
